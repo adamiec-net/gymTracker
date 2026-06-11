9bbb7b9769ee9648a46d7a68539b016ed6842157
Tomasz Adamiec <adamiec.net@outlook.com>
2026-06-11 06:41:04 +0200

    feat: publish

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -2,7 +2,7 @@ export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/sw.js')
+        .register('./sw.js')
         .then((registration) => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
